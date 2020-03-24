e62dcb7b32c8693b616c76ea89f1edf831df194d
Matthias Utz <utz.matthias@web.de>
2020-03-24 20:58:34 +0100

    Delete SeiteAktualisieren.sh

--- SeiteAktualisieren.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo -i
-git reset --hard HEAD
-cd /home/pi/Internetseite/
-git pull https://github.com/mutz025/Internetseite.git
-rsync --delete -a /home/pi/Internetseite/ /var/www/html/
